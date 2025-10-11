2d8ec2061e7e028838ec9b3407698dc789891dbb
Aman Kumar <aman.kumar@geeksforgeeks.org>
2025-10-11 17:12:13 +0530

    add:WE-18

--- backend/WE-18/server.js
+++ backend/WE-18/server.js
@@ -9,8 +9,9 @@ const PORT = 3000 || 3001
 
 app.use(express.json())
 
+//mongodb+srv://dbUser:Aman1234@project1.n9qjal6.mongodb.net/mern_16(mention database name)
 try {
-    mongoose.connect('mongodb+srv://dbUser:Aman1234@project1.n9qjal6.mongodb.net/')
+    mongoose.connect('mongodb+srv://dbUser:Aman1234@project1.n9qjal6.mongodb.net/mern_16')
     console.log('database connected');
 }
 catch (err) {
